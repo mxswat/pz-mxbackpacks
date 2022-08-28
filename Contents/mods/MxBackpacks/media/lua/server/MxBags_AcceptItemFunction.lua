cd4628a6509784e72159c4ad1ece9cbec564a478
mxswat <massimocacciapaglia.13@gmail.com>
2022-08-29 00:45:24 +0100

    Specialized bag first iteration, works

---
+++ Contents/mods/MxBackpacks/media/lua/server/MxBags_AcceptItemFunction.lua
@@ -0,0 +1,7 @@
+-- This is based off media\lua\server\Items\AcceptItemFunction.lua
+
+MxAcceptItemFunction = MxAcceptItemFunction or {}
+
+function MxAcceptItemFunction.MedicalOnly(container, item)
+	return item:getStringItemType() == "Medical"
+end
