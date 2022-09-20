22baa5e1855efcd497a2d7851e77f3b35046b77c
mxswat <massimocacciapaglia.13@gmail.com>
2022-09-20 23:52:40 +0100

    Compatibity fix with "They Knew"

--- Contents/mods/MxBackpacks/media/lua/server/MxBags_AcceptItemFunction.lua
+++ Contents/mods/MxBackpacks/media/lua/server/MxBags_AcceptItemFunction.lua
@@ -3,5 +3,5 @@
 MxAcceptItemFunction = MxAcceptItemFunction or {}
 
 function MxAcceptItemFunction.MedicalOnly(container, item)
-	return item:getStringItemType() == "Medical"
+	return item:getStringItemType() == "Medical" or item.Medical
 end
